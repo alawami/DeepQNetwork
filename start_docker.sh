5d9a5017f33e789c66d2ff5b3de71939876138d9
alawami <ahmed.alawami+github@alumni.colostate.edu>
2020-06-16 07:14:12 -0600

    imporve start_docker.sh

--- start_docker.sh
+++ start_docker.sh
@@ -1,1 +1,1 @@
-docker run --gpus all --rm -it -v "$(pwd)":/rl -p 8787:8787 -p 8786:8786 -p 8888:8888 aalawami/banana_rl:cuda10.2 --
+docker run --gpus all --name banana --rm -it -v "$(pwd)":/rl -p 8787:8787 -p 8786:8786 -p 8888:8888 --ulimit stack=67108864 --ulimit memlock=-1 aalawami/banana_rl:cuda10.2 --
